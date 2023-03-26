1d9a04d5cf3d1c906a350769135ad9d0d7f2d399
DariaKuzmenko <103844653+DariaKuzmenko@users.noreply.github.com>
2023-03-26 23:33:03 +0200

    Commit all tasks

---
+++ src/js/02-video.js
@@ -0,0 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+setCurrentTime();
+
+const handleCurrentTime = el => {
+  localStorage.setItem(STORAGE_KEY, el.seconds);
+};
+
+player.on('timeupdate', throttle(handleCurrentTime), 1000);
+
+function setCurrentTime() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+formEl.addEventListener('input', throttle(onInputSubmit), 500);
+formEl.addEventListener('submit', onFormSubmit);
+
+let formText = {};
+
+populateForm();
+
+function onInputSubmit(event) {
+  formText[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formText));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (formEl.email.value === '' || formEl.message.value === '') {
+    alert('Заповни поля!!!');
+    return;
+  }
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formText);
+  event.currentTarget.reset();
+  formText = {};
+}
+
+function populateForm() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessage) {
+    formText = savedMessage;
+
+    for (const key in formText) {
+      formEl.elements[key].value = formText[key];
+    }
+  }
+}
